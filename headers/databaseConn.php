b5153454d481eef4e541c405a2a21c568aeaf661
Sagar Anand <sagar.anand015@gmail.com>
2015-12-22 09:59:22 +0530

    Changed the db credentials to work on the linux hosting.

--- headers/databaseConn.php
+++ headers/databaseConn.php
@@ -1,11 +1,11 @@
 <?php 
 
 	//to make the database connection here!
-	$connection=mysql_connect("50.62.209.154","theMRFinal","Mresearch123");
+	$connection=mysql_connect("localhost","mrdb","MentoredResearch");
 	if(!$connection) {
 	    die("Error Establishing connection");
 	}
-	$db = mysql_select_db("courses_",$connection);
+	$db = mysql_select_db("mrcourses",$connection);
 	if(!$db) {
 	    die("Cannot select the database");
 	}
